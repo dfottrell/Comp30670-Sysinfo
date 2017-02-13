c8989830fcf05ff20b0742dde90660b4c354955a
David Fottrell <davefottrell@gmail.com>
2017-02-13 14:01:11 +0000

    Added extra functionality to pull more details on system

--- Sysinfo/main.py
+++ Sysinfo/main.py
@@ -7,6 +7,12 @@ import platform
 
 def main():
     print(platform.platform())
+    print("The name of this machine is: ", platform.machine())
+    print("The processor is: ", platform.processor())
+    print("The operating system is: ", platform.system())
+    print("The version of this system is: ", platform.release())
+    print("The version of this operating system is: ", platform.version())
+    print("The user name of this system is: ", platform.uname())
     return
 
 
